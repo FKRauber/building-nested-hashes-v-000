0dd45cab879772f6f7c3ce359336e63409a88db8
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-05 04:39:17 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,13 +24,11 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        {
-          name: "Steven",
+        { name: "Steven",
           age: "30",
           attitude: "confused"
         },
-        {
-          name: "Nurse",
+        { name: "Nurse",
           age: "44",
           attitude: "worried"
         },
