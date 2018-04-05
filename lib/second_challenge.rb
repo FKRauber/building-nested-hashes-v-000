9bd1c150afd75d40700c3c1aaff6426ffb6875d7
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-05 04:24:10 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,14 @@ def second_challenge
   epic_tragedy = {
     montague: {
       #your key/value pairs here!
-      }, 
+      matriarch:
+      patriarch:
+      hero:
+      hero_friends:
+      },
     capulet: {
       #your key/value pairs here!
       }}
 
-  
+
 end
